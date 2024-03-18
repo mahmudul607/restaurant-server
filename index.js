1a9f6a808c49af9b32667f52719545338d7f67d2
unknown <mahmudul.cse.eng@gmail.com>
2024-03-19 00:39:33 +0600

    payment intent api created successful

--- index.js
+++ index.js
@@ -73,11 +73,14 @@ async function run() {
     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
-
+      console.log(amount, 'amount inside the intent');
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: 'USD',
+        currency: 'usd',
         payment_method_types: ['card']
+      },
+      {
+        apiKey:process.env.STRIPE_SECRET_KEY
       });
 
       res.send({
